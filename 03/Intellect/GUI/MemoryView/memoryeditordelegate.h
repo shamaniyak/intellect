d9dcfee4b2536668146a8d5206e6e57ef40fd3a7
shamaniyak <shamaniyak@yandex.ru>
2017-08-18 01:19:08 +0300

    Делегат для MemoryView

--- 03/Intellect/GUI/MemoryView/memoryeditordelegate.h
+++ 03/Intellect/GUI/MemoryView/memoryeditordelegate.h
@@ -2,12 +2,23 @@
 #define MEMORYEDITORDELEGATE_H
 
 #include <QObject>
+#include <QStyledItemDelegate>
 
-class MemoryEditorDelegate : public QObject
+class MemoryEditorDelegate : public QStyledItemDelegate
 {
   Q_OBJECT
 public:
   explicit MemoryEditorDelegate(QObject *parent = 0);
+
+  QWidget *createEditor(QWidget *parent, const QStyleOptionViewItem &option,
+                        const QModelIndex &index) const override;
+
+  void setEditorData(QWidget *editor, const QModelIndex &index) const override;
+  void setModelData(QWidget *editor, QAbstractItemModel *model,
+                    const QModelIndex &index) const override;
+
+  void updateEditorGeometry(QWidget *editor,
+      const QStyleOptionViewItem &option, const QModelIndex &index) const override;
 
 signals:
 
