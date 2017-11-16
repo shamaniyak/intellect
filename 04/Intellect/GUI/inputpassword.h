9ffd469c9ee874f6e41c5e01991ebb86eb91d020
shamaniyak <shamaniyak@yandex.ru>
2017-11-16 22:44:03 +0300

    Доработан виджет ввода пароля.

--- 04/Intellect/GUI/inputpassword.h
+++ 04/Intellect/GUI/inputpassword.h
@@ -1,17 +1,30 @@
 #ifndef INPUTPASSWORD_H
 #define INPUTPASSWORD_H
 
-#include <QWidget>
+#include <QDialog>
 
-class InputPassword : public QWidget
+class QLineEdit;
+
+class InputPassword : public QDialog
 {
   Q_OBJECT
 public:
   explicit InputPassword(QWidget *parent = nullptr);
 
+  bool isTrue();
+
 signals:
 
 public slots:
+  int exec();
+
+private:
+  QString pass_;
+  QLineEdit *edt_ = nullptr;
+
+  // QWidget interface
+protected:
+  void keyPressEvent(QKeyEvent *event) override;
 };
 
 #endif // INPUTPASSWORD_H
