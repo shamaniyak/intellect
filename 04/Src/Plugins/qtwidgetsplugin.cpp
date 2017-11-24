615815fbdacaf31814a7dfe99f0f1a555456f31d
shamaniyak <shamaniyak@yandex.ru>
2017-11-24 15:46:32 +0300

    Изменен qtwidgetsplugin.cpp.

--- 04/Src/Plugins/qtwidgetsplugin.cpp
+++ 04/Src/Plugins/qtwidgetsplugin.cpp
@@ -16,6 +16,36 @@ QScriptValue QMainWindowToScriptValue(QScriptEngine *engine, QMainWindow* const 
 void QMainWindowFromScriptValue(const QScriptValue &object, QMainWindow* &out)
 { out = qobject_cast<QMainWindow*>(object.toQObject()); }
 
+//  Qt::MouseButton
+
+Q_DECLARE_METATYPE(Qt::MouseButton)
+
+QScriptValue QtMouseButtonToScriptValue(QScriptEngine *engine, Qt::MouseButton const &in)
+{ return engine->newVariant(QVariant(int(in))); }
+
+void QtMouseButtonFromScriptValue(const QScriptValue &object, Qt::MouseButton &out)
+{ out = (Qt::MouseButton)(object.toInt32()); }
+
+//  QPointF
+
+Q_DECLARE_METATYPE(QPointF)
+
+QScriptValue QPointFToScriptValue(QScriptEngine *engine, QPointF const &in)
+{
+  QScriptValue obj = engine->newObject();
+  obj.setProperty("x", in.x());
+  obj.setProperty("y", in.y());
+  return obj;
+}
+
+void QPointFFromScriptValue(const QScriptValue &object, QPointF &out)
+{
+  out.setX(object.property("x").toNumber());
+  out.setY(object.property("y").toNumber());
+}
+
+// QtWidgetsPlugin
+
 QtWidgetsPlugin::QtWidgetsPlugin(QScriptEngine *engine)
 {
   if(engine){
@@ -35,5 +65,6 @@ QtWidgetsPlugin::QtWidgetsPlugin(QScriptEngine *engine)
 //    engine->globalObject().setProperty("QWidget", metaObj);
 
     qScriptRegisterMetaType(engine, QMainWindowToScriptValue, QMainWindowFromScriptValue);
+    qScriptRegisterMetaType(engine, QtMouseButtonToScriptValue, QtMouseButtonFromScriptValue);
   }
 }
