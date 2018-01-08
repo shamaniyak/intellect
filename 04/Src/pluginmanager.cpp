61d796c1dc6a8bb5c25bf9b7637b9cb5f79dfd60
shamaniyak <shamaniyak@yandex.ru>
2018-01-08 23:38:24 +0300

    Добавлена возможность загружать и выгружать отдельный плагин.

--- 04/Src/pluginmanager.cpp
+++ 04/Src/pluginmanager.cpp
@@ -1,8 +1,8 @@
 #include "pluginmanager.h"
 
+#include <QtDebug>
 #include <QDir>
 #include <QApplication>
-#include <QMessageBox>
 #include <QPluginLoader>
 
 PluginManager::PluginManager(QObject *parent) : QObject(parent)
@@ -12,31 +12,33 @@ PluginManager::PluginManager(QObject *parent) : QObject(parent)
 
 PluginManager::~PluginManager()
 {
-
+  clear();
 }
 
 void PluginManager::clear()
 {
-
+  for(auto &&obj: children())
+  {
+    auto p = qobject_cast<QPluginLoader*>(obj);
+    if(p)
+      p->unload();
+  }
 }
 
 void PluginManager::loadPlugins()
 {
   QDir dir(QApplication::applicationDirPath());
   if (!dir.cd("plugins")) {
-    QMessageBox::critical(0, "", "plugins directory does not exist");
+    qDebug() << "PluginManager: plugins directory does not exist";
     return;
   }
-  foreach (QString fileName, dir.entryList(QDir::Files)) {
-    QPluginLoader loader(dir.absoluteFilePath(fileName));
-    addPlugin(qobject_cast<QObject*>(loader.instance()));
+  for (QString fileName: dir.entryList(QDir::Files)) {
+    // Уберем расширение
+    auto pos = fileName.lastIndexOf('.');
+    if(pos > -1)
+      fileName = fileName.left(pos);
+    loadPlugin(dir, fileName);
   }
-}
-
-void PluginManager::addPlugin(QObject *pobj)
-{
-  if(pobj)
-    pobj->setParent(this);
 }
 
 int PluginManager::count() const
@@ -47,11 +49,62 @@ int PluginManager::count() const
 QObject *PluginManager::get(unsigned int i) const
 {
   if(i < (unsigned int)count())
-    return(this->children()[i]);
+  {
+    auto pl = qobject_cast<QPluginLoader*>(this->children()[i]);
+    if(pl)
+      return(pl->instance());
+  }
   return nullptr;
 }
 
 QObject *PluginManager::getByName(const QString &name)
 {
-  return(this->findChild<QObject*>(name) );
+  auto pl = this->findChild<QPluginLoader*>(name);
+  return( pl ? pl->instance() : nullptr );
+}
+
+QObject *PluginManager::loadPlugin(const QString &name)
+{
+  auto pobj = getByName(name);
+  if(pobj)
+    return pobj;
+
+  QDir dir(QApplication::applicationDirPath());
+  if (!dir.cd("plugins")) {
+    qDebug() << "PluginManager: plugins directory does not exist";
+    return nullptr;
+  }
+
+  return loadPlugin(dir, name);
+}
+
+bool PluginManager::unloadPlugin(const QString &name)
+{
+  auto pobj = this->findChild<QPluginLoader*>(name);
+  if(pobj) {
+    if(pobj->unload()) {
+      delete pobj;
+      return true;
+    }
+  }
+  return false;
+}
+
+void PluginManager::addPlugin(QObject *pobj)
+{
+  pobj = qobject_cast<QObject*>(pobj);
+  if(pobj)
+    pobj->setParent(this);
+}
+
+QObject *PluginManager::loadPlugin(const QDir &dir, const QString &name)
+{
+  QPluginLoader *loader = new QPluginLoader(dir.absoluteFilePath(name));
+  auto pobj = loader->instance();
+  if(pobj)
+  {
+    loader->setObjectName(name);
+    loader->setParent(this);
+  }
+  return pobj;
 }
--- 04/Src/pluginmanager.h
+++ 04/Src/pluginmanager.h
@@ -3,6 +3,8 @@
 
 #include <QObject>
 
+class QDir;
+
 class PluginManager : public QObject
 {
   Q_OBJECT
@@ -14,18 +16,21 @@ public:
 
   void loadPlugins();
 
-  void addPlugin(QObject *pobj);
-
 signals:
 
 public slots:
   int count() const;
   QObject *get(unsigned int i) const;
   QObject *getByName(const QString &name);
+  QObject *loadPlugin(const QString &name);
+  bool unloadPlugin(const QString &name);
 
 private:
   typedef std::vector<QObject*> Plugins;
   Plugins plugins_;
+
+  void addPlugin(QObject *pobj);
+  QObject *loadPlugin(const QDir &dir, const QString &name);
 };
 
 #endif // PLUGINMANAGER_H
